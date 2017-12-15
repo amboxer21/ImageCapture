ea69c52dd60a243c679aa9ebf328e65aaac29432
amboxer21 <amboxer21@gmail.com>
2017-12-15 13:24:07 -0500

    Added checkbox state function.

--- ImageCaptureUI.py
+++ ImageCaptureUI.py
@@ -6,12 +6,13 @@ import gtk
 
 class ImageCaptureUI:
 
-  def hello(self, widget, data=None):
-    print "Hello World!"
+  def clicked(self, widget, data=None):
+    print "Clicked!"
 
   def is_checked(self, widget, data=None):
-    checked = str(widget.get_active())
-    print "Auto Start Enabled!"
+    if widget.get_active():
+      print str(data) + " enabled!"
+      return str(data)
 
   def delete_event(self, widget, event, data=None):
     print "Delete event occured."
@@ -27,21 +28,25 @@ class ImageCaptureUI:
     self.window.set_title("ImageCapture")
     self.window.set_border_width(10)
 
-    table    = gtk.Table(10, 10, True)
-    button   = gtk.Button("Hello World")
-    checkbox = gtk.CheckButton(label='Auto Start', use_underline=False)
+    table         = gtk.Table(10, 10, True)
+    button        = gtk.Button("Hello World")
+    checkboxCam   = gtk.CheckButton(label='Enable Cam', use_underline=False)
+    checkboxLogin = gtk.CheckButton(label='Auto Login', use_underline=False)
 
     self.window.add(table)
     table.attach(button, 0, 1, 0, 1)
-    table.attach(checkbox, 0, 1, 1, 2)
+    table.attach(checkboxCam, 0, 1, 1, 2)
+    table.attach(checkboxLogin, 1, 2, 1, 2)
 
-    button.connect("clicked", self.hello, None)
+    button.connect("clicked", self.clicked, None)
     button.connect_object("clicked", gtk.Widget.destroy, self.window)
-    checkbox.connect("toggled", self.is_checked, None)
+    checkboxCam.connect("toggled", self.is_checked, "enable_cam")
+    checkboxLogin.connect("toggled", self.is_checked, "auto_login")
   
     table.show()
     button.show()
-    checkbox.show()
+    checkboxCam.show()
+    checkboxLogin.show()
     self.window.show()
 
   def main(self):
