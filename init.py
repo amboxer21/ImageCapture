e60991c1a5c02ea0d0cb0b769c048de8038d74f7
amboxer21 <amboxer21@gmail.com>
2018-06-14 10:52:34 -0400

    Added prompt ui for initializing imagecapture for the first time.

--- init.py
+++ init.py
@@ -1,6 +1,27 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
+from Tkinter import *
 
-from tkinter import messagebox
- 
-messagebox.showinfo("ImageCapturePy", "Initialize ImageCapture?")
+WELCOME_DURATION = 2000
+INFO = '''ImageCapturePy must be initialized before you can use it for the first time.'''
+
+def init():
+    top = Toplevel()
+    top.title('ImageCapturePy')
+    Message(top, text=WELCOME_MSG, padx=20, pady=20).pack()
+    top.after(WELCOME_DURATION, top.destroy)
+
+window = Tk()
+window.geometry("150x150") 
+window.resizable(0,0)
+window.title('ImageCapturePy')
+
+msg   = Frame(window)
+msg.pack(side=TOP)
+Message(msg, text=INFO, padx=20, pady=20).pack()
+
+bttns = Frame(window)
+bttns.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+Button(bttns, text="OK", fg="Green", command=init).pack(side=LEFT, fill=BOTH, expand=True)
+Button(bttns, text="Cancel", fg="Red", command=init).pack(side=RIGHT, fill=BOTH, expand=True)
+
+window.mainloop()
