4bdf0317fa5a0456677a0ee89cd63ae5a08e974c
amboxer21 <amboxer21@gmail.com>
2018-07-13 08:40:41 -0400

    Fixed logging calls missing 'NEW' missing args.

--- imagecapturepy/modules/db/db.py
+++ imagecapturepy/modules/db/db.py
@@ -15,11 +15,11 @@ def writeToDB(location_bool, coordinates, ip_addr):
     if location_bool is None or coordinates is None or ip_addr is None:
         return
     elif not re.search("true|false|NULL", location_bool, re.I|re.M):
-        logger.log(str(location_bool) + " is not a known mode.")
+        logger.log("[ERROR]", str(location_bool) + " is not a known mode.")
     elif not re.search("\A\((\d|\-\d)+\.\d+,\s(\d|\-\d)+\.\d+\)|NULL", coordinates, re.M | re.I): 
-        logger.log("Improper coordinate format -> " + str(coordinates) + ".")
+        logger.log("[ERROR]", "Improper coordinate format -> " + str(coordinates) + ".")
     elif not re.search("\A\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$|NULL", ip_addr, re.M|re.I):
-        logger.log("Improper ip address format -> " + str(ip_addr) + ".") 
+        logger.log("[ERROR]", "Improper ip address format -> " + str(ip_addr) + ".") 
     else:
         coor = re.sub("[\(\)]", "", str(coordinates))
         db.execute("insert into connected (location_bool, coordinates, ip_addr) values(\"" + str(location_bool) + "\", \"" + str(coor) + "\", \"" + str(ip_addr) + "\")")
@@ -35,7 +35,7 @@ def readFromDB(column):
         elif column == 'ip_addr' and row[3] is not None:
             return str(row[3])
         else:
-            logger.log("Not a known column or DB is empty.") 
+            logger.log("[ERROR]", "Not a known column or DB is empty.") 
             return
 
 def updateDB(column,value):
@@ -43,7 +43,7 @@ def updateDB(column,value):
         return
     try:
         if readFromDB('location_bool') is None or readFromDB('coordinates') is None or readFromDB('ip_addr') is None:
-            logger.log("You must write to the database first before updating!")
+            logger.log("[ERROR]", "You must write to the database first before updating!")
             return
         elif re.search("true|false", value, re.I|re.M) and column == 'location_bool':
             db.execute("update connected set location_bool = \"" + value + "\"")
@@ -55,52 +55,52 @@ def updateDB(column,value):
             db.execute("update connected set ip_addr = \"" + value + "\"")
             db.commit()
         else:
-            logger.log(str(column) + " is not a known column for the connected table in the imagecapture db.")
+            logger.log("[ERROR]", str(column) + " is not a known column for the connected table in the imagecapture db.")
             return
     except sqlite3.OperationalError:
-      logger.log("The database is lock, could not add coordinates to DB.")
+      logger.log("[ERROR]", "The database is lock, could not add coordinates to DB.")
 
 def addLocationToDB(location_bool):
     try:
         if readFromDB('location_bool') is None:
             writeToDB(location_bool,'NULL','NULL')
-            logger.log("Writing location_bool to DB.")
+            logger.log("[INFO]", "Writing location_bool to DB.")
         elif readFromDB('location_bool') != location_bool and readFromDB('location_bool') is not None:
             updateDB('location_bool', location_bool)
-            logger.log("Updating location_bool variable in DB.")
+            logger.log("[INFO]", "Updating location_bool variable in DB.")
         else:
             return
     except sqlite3.OperationalError:
         call(['/usr/bin/rm', '/home/' + user.name() + '/.imagecapture/imagecapture.db'])
-        logger.log("The database is locked, could not add location_bool to DB.")
+        logger.log("[ERROR]", "The database is locked, could not add location_bool to DB.")
         pass
 
 def addCoordinatesToDB(coordinates):
     try:
         if readFromDB('coordinates') is None:
             writeToDB('NULL', coordinates,'NULL')
-            logger.log("Writing coordinates to DB.")
+            logger.log("[INFO]", "Writing coordinates to DB.")
         elif not readFromDB('coordinates') == coordinates and readFromDB('coordinates') is not None:
             updateDB('coordinates', ip_addr)
-            logger.log("Updating coordinates variable in DB.")
+            logger.log("[INFO]", "Updating coordinates variable in DB.")
         else:
             return
     except sqlite3.OperationalError:
         call(['/usr/bin/rm', '/home/' + user.name() + '/.imagecapture/imagecapture.db'])
-        logger.log("The database is locked, could not add coordinates to DB.")
+        logger.log("[ERROR]", "The database is locked, could not add coordinates to DB.")
         pass
 
 def addIpToDB(ip_addr):
     try:
         if readFromDB('ip_addr') is None:
             writeToDB('NULL','NULL', ip_addr)
-            logger.log("Writing ip_addr to DB.")
+            logger.log("[INFO]", "Writing ip_addr to DB.")
         elif readFromDB('ip_addr') != ip_addr and readFromDB('ip_addr') is not None:
             updateDB('ip_addr', ip_addr)
-            logger.log("Updating ip_addr variable in DB.")
+            logger.log("[INFO]", "Updating ip_addr variable in DB.")
         else:
             return
     except sqlite3.OperationalError:
         call(['/usr/bin/rm', '/home/' + user.name() + '/.imagecapture/imagecapture.db'])
-        logger.log("The database is locked, could not add IP address to DB.")
+        logger.log("[ERROR]", "The database is locked, could not add IP address to DB.")
         pass
