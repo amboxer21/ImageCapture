14e985cd9d5cc8ad2fd16a7396c00b7b1d2d182c
amboxer21 <amboxer21@gmail.com>
2018-08-14 11:23:56 -0400

    Fixed bu

--- src/imagecapture.py
+++ src/imagecapture.py
@@ -688,7 +688,7 @@ class FileOpts():
         return "/home/" + str(user.name()) + "/.imagecapture"
 
     def picture_directory(self):
-        return self.root_directory + "/pictures"
+        return str(self.root_directory()) + "/pictures"
 
     def picture_path(self):
         return str(self.picture_directory()) + '/capture.png'
