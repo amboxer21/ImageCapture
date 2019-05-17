84e33730448db8cb5e29dc79be4c15be70863245
amboxer21 <amboxer21@gmail.com>
2019-05-17 15:10:07 -0400

    Fixed up and refactored getlocations classes run method.

--- src/imagecapture.py
+++ src/imagecapture.py
@@ -462,7 +462,6 @@ class GetLocation(Thread):
 
     def __init__(self,website,email,browser):
         Thread.__init__(self)
-        self.count = 0
         self._email_,config_dict[0]['email'][0]   = (email,email)
         self._website_,config_dict[0]['website'][0] = (website,website)
         self._browser_,config_dict[0]['browser'][0] = (browser,browser)
@@ -472,38 +471,33 @@ class GetLocation(Thread):
 
     def run(self):
 
+        browser = None
+
         # This is the supported browser list and can be added to.
         browsers = [
             self._browser_,
             '/opt/google/chome/chrome',
-            '/usr/bin/opera']
+            '/usr/bin/opera'
+        ]
 
         for b in browsers: 
-            if self.browser_exists(b) is not None and self.count == 0:
-                _browser_ = re.match("(\/\w+)(.*\/)(\w+)",self._browser_).group(3)
+            if self.browser_exists(b) is not None:
+                browser = re.match("(\/\w+)(.*\/)(\w+)",b).group(3)
                 break
-            self.count += 1
-            if self.count > len(b):
-                logger.log("ERROR",
-                    "Only the following browsers are supported: Chrome, Opera, and Firefox.")
-            elif self.browser_exists(b):
-                _browser_ = re.match("(\/\w+)(.*\/)(\w+)",b).group(3)
-                break
-        print('=> browser -> '+str(_browser_))
-        if _browser_ == 'chrome':
-            call([_browser_, "--user-data-dir="
+        if browser is None:
+            logger.log("WARNING",
+                "Browser not found and location functionality will not work.")
+        elif browser == 'chrome':
+            call([browser, "--user-data-dir="
                 + str(fileOpts.root_directory()), "--no-sandbox", ""
                 + self._website_
                 + "?Email=" + self._email_])
-        elif _browser_ == 'opera':
-            call([_browser_, "--user-data-dir="
+        elif browser == 'opera':
+            call([browser, "--user-data-dir="
                 + str(fileOpts.root_directory()), "--no-sandbox", ""
                 + self._website_
                 + "?Email=" + self._email_])
-        else:
-            logger.log("WARNING", "\n\nBrowser not "
-                + "found and location functionality will not work.\n\n")
-            sys.sleep(2)
+            time.sleep(2)
 
 class Database():
 
