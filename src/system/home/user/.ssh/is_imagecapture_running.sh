8002c97ba507f9b206d9155d0ca814e02fa455e1
amboxer21 <amboxer21@gmail.com>
2019-05-19 19:01:25 -0400

    Fixed up the imagecapture monitor daemon.

--- src/system/home/user/.ssh/is_imagecapture_running.sh
+++ src/system/home/user/.ssh/is_imagecapture_running.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/bin/python /usr/local/bin/imagecapture.py" | /usr/bin/wc -l` < 1 ]]; then
+if [[ `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/.*/pyth.* /usr/local/bin/imagecapture.py" | /usr/bin/wc -l` < 1 ]]; then
   logger -i -t imagecapture "Imagecapture is not running, starting imagecapture."
-  /usr/bin/python /usr/local/bin/imagecapture.py -C;
-  /usr/bin/python /usr/local/bin/imagecapture.py -ALUvce 'sshmonitorapp@gmail.com' -p 'hkeyscwhgxjzafvj' &
-elif [[ `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/bin/python /usr/local/bin/imagecapture.py" | /usr/bin/wc -l` > 1 ]]; then
+  /usr/bin/python /usr/local/bin/imagecapture.py -X;
+  /usr/bin/python /usr/local/bin/imagecapture.py -ALUvc -l '/var/log/messages' -e 'sshmonitorapp@gmail.com' -p 'hkeyscwhgxjzafvj' &
+elif [[ `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/.*/pyth.* /usr/local/bin/imagecapture.py" | /usr/bin/wc -l` > 1 ]]; then
   logger -i -t imagecapture "Restarting imagecapture."
-  /usr/bin/sudo /bin/kill -9 `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/bin/python /usr/local/bin/imagecapture.py" | /usr/bin/awk '{print $2}'`;
-  /usr/bin/python /usr/local/bin/imagecapture.py -C;
-  /usr/bin/python /usr/local/bin/imagecapture.py -ALUvce 'sshmonitorapp@gmail.com' -p 'hkeyscwhgxjzafvj' &
+  /usr/bin/sudo /bin/kill -9 `/bin/ps aux | /bin/egrep -i "[0-9]{1,2}:[0-9]{1,2} /usr/.*/pyth.* /usr/local/bin/imagecapture.py" | /usr/bin/awk '{print $2}'`;
+  /usr/bin/python /usr/local/bin/imagecapture.py -X;
+  /usr/bin/python /usr/local/bin/imagecapture.py -ALUvc -l '/var/log/messages' -e 'sshmonitorapp@gmail.com' -p 'hkeyscwhgxjzafvj' &
 else
   logger -i -t imagecapture "Imagecapture is running.";
 fi
